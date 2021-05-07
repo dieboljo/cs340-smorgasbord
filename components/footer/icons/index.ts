a81651e32399127db9c253ad5aa6b1d7e065b568
J Max Diebold <maxdbold@gmail.com>
2021-05-07 04:19:06 -0400

    Removed pesky icon index.

--- components/footer/icons/index.ts
+++
@@ -1,6 +0,0 @@
-// components/footer/icons
-
-export { default as Facebook } from "./facebook.svg"
-export { default as OpenTable } from "./openTable.svg"
-export { default as Twitter } from "./twitter.svg"
-export { default as YouTube } from "./youTube.svg"
