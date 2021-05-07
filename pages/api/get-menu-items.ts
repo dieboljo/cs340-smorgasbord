b69a55cbed82a357223ea985de366218009832ff
J Max Diebold <maxdbold@gmail.com>
2021-05-07 04:34:35 -0400

    One problem file.

---
+++ pages/api/get-menu-items.ts
@@ -0,0 +1,48 @@
+import { NextApiHandler } from 'next'
+import { query } from '../../lib/db'
+
+const handler: NextApiHandler = async (req, res) => {
+  const { id } = req.query
+  try {
+    if (!id) {
+      return res.status(400).json({ message: '`id` required' })
+    }
+    if (typeof parseInt(id.toString()) !== 'number') {
+      return res.status(400).json({ message: '`id` must be a number' })
+    }
+    const results = await query(
+      `
+      SELECT id, title, content
+      FROM entries
+      WHERE id = ?
+    `,
+      id
+    )
+
+    return res.json(results[0])
+  } catch (e) {
+    res.status(500).json({ message: e.message })
+  }
+}
+
+const handlerSample: NextApiHandler = async (req, res) => {
+    const results= [
+        {
+            menuItemId: 43,
+            name: "Beef Taco",
+            description: "A taco filled with beef",
+            price: 14.20,
+            location: 34
+        },
+    ]
+    const error = false
+
+    return {
+        menuItems: results,
+        isLoading: !error && !results,
+        isError: error,
+    }
+    return res.json(results);
+}
+
+export default handlerSample
