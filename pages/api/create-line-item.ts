910d4fba98ec7922afe3be332bda3ff0638bcab1
kj128 <62865805+kj128@users.noreply.github.com>
2021-05-19 21:03:09 -0700

    Update create-line-item.ts

--- pages/api/create-line-item.ts
+++ pages/api/create-line-item.ts
@@ -14,10 +14,7 @@ const handler: NextApiHandler = async (req, res) => {
     }
 
     const results = await query(
-      `
-      INSERT INTO entries (title, content)
-      VALUES (?, ?)
-      `,
+      INSERT INTO LineItems (`order`, quantity, menuItem) VALUES ($lineItemQuantity, $menuItemId), 
       [filter.clean(name), filter.clean(email)]
     )
 
