946d9380a62d1c23af3d9a7e8a0779cbce1ec82d
kj128 <62865805+kj128@users.noreply.github.com>
2021-05-19 21:02:10 -0700

    Update create-customer.ts

--- pages/api/create-customer.ts
+++ pages/api/create-customer.ts
@@ -14,10 +14,7 @@ const handler: NextApiHandler = async (req, res) => {
     }
 
     const results = await query(
-      `
-      INSERT INTO entries (title, content)
-      VALUES (?, ?)
-      `,
+      INSERT INTO Customers (name, email) VALUES ($customerName, $customerEmail),
       [filter.clean(name), filter.clean(email)]
     )
 
